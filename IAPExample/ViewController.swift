55590c3edcb41da79012dce7ca5b202d21d45a0e
Thi Nguyen Tam <nguyentamthi11@gmail.com>
2019-02-27 07:12:24 +0700

    add ui

--- IAPExample/ViewController.swift
+++ IAPExample/ViewController.swift
@@ -18,3 +18,14 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
+
